80e29fcd9c68e91ebe822ae6086aa9d0d213df0f
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-06-15 23:33:29 +0600

    add objects

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const subject = ['software','electrical','mechanical','civil'];
+const emgineering = ['software','electrical','mechanical','civil'];
+const bio_science = ['microbiology','bio-technology','bio-chemistry','genetics-engineering'];
+const pure_science = ['physics','chemistry','mathematics','statistics'];
+const applied_science = ['chemical-science','applied-mathematics','environmental-science','applied-physices'];
 
 
-module.exports=subject;
+module.exports={
+    emgineering,pure_science,bio_science,applied_science
+};
